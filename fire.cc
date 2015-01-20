759c772b752c4dc1de20d17edb425a4c72c2144d
Simon Thiel <simon.thiel@gmx.de>
2015-01-20 16:22:13 +0100

    added daemon mode

--- fire.cc
+++ fire.cc
@@ -1,9 +1,5 @@
 // -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
-// Small example how to use the library.
-// For more examples, look at demo-main.cc
-//
-// This code is public domain
-// (but note, that the led-matrix library this depends on is GPL v2)
+
 
 #include "led-matrix.h"
 #include "threaded-canvas-manipulator.h"
@@ -26,6 +22,16 @@ using rgb_matrix::Canvas;
 
 #define WIDTH 32
 #define HEIGHT 32
+
+static int usage(const char *progname) {
+    fprintf(stderr, "usage: %s [-d]\n", progname);
+    fprintf(stderr, "Options:\n"
+            "\t-d            : run as daemon. Use this when starting in\n"
+            "\t                /etc/init.d, but also when running without\n"
+            "\t                terminal (e.g. cron).\n");
+
+    return 1;
+}
 
 class FireManipulator : public ThreadedCanvasManipulator {
 private:
@@ -50,6 +56,7 @@ private:
 public:
 
     FireManipulator(Canvas *m) : ThreadedCanvasManipulator(m) {
+
         counter = 1;
 
         seed = static_cast<int> (time(0));
@@ -61,9 +68,11 @@ public:
         seed4 = randomfloat(-.04, .030);
         seed5 = randomfloat(0.2, 3);
         seed6 = randomfloat(0.2, 3);
+
     }
 
     void Run() {
+
         //clear matrix
         canvas()->Fill(0, 0, 0);
         while (running()) {
@@ -100,13 +109,23 @@ public:
     }
 };
 
-int main(int argc, char *argv[]) {
+static RGBMatrix *matrix;
+
+static Canvas *canvas;
+static ThreadedCanvasManipulator *image_gen;
+static GPIO io;
+
+static int setupAndStartFire() {
+
     /*
      * Set up GPIO pins. This fails when not running as root.
      */
-    GPIO io;
-    if (!io.Init())
+
+    if (!io.Init()) {
+
+        fprintf(stderr, "io.Init() failed!\n");
         return 1;
+    }
 
     /*
      * Set up the RGBMatrix. It implements a 'Canvas' interface.
@@ -115,19 +134,79 @@ int main(int argc, char *argv[]) {
     int chain = 1; // Number of boards chained together.
     bool do_luminance_correct = true;
     // The matrix, our "frame buffer" and display updater.
-    RGBMatrix *matrix = new RGBMatrix(&io, rows, chain);
+    matrix = new RGBMatrix(&io, rows, chain);
     matrix->set_luminance_correct(do_luminance_correct);
-    Canvas *canvas = matrix;
-    ThreadedCanvasManipulator *image_gen = new FireManipulator(canvas);
+    canvas = matrix;
+    image_gen = new FireManipulator(canvas);
     // Now start the thread.
+
     image_gen->Start();
 
-    // Things are set up. Just wait for <RETURN> to be pressed.
-    printf("Press <RETURN> to exit and reset LEDs\n");
-    getchar();
+    return 0;
+}
 
-    delete image_gen;
-    delete canvas;
+int main(int argc, char *argv[]) {
 
+
+    bool as_daemon = false;
+    int opt;
+
+
+    while ((argc > 0) //skip option check in case called without command line arguments
+            &&((opt = getopt(argc, argv, "dlD:t:r:p:c:m:L")) != -1)) {
+        switch (opt) {
+
+            case 'd':
+                as_daemon = true;
+                break;
+
+            default: /* '?' */
+                return usage(argv[0]);
+        }
+    }
+
+
+
+    if (as_daemon) {
+        pid_t pID = fork();
+
+        if (pID != 0) {
+            if (pID < 0) {
+                fprintf(stderr, "error on becoming daemon: %i\n", pID);
+                return 1;
+            }
+            fprintf(stdout, "%i\n", pID);
+            return 0;
+        }
+        //fprintf(stderr, "daemon mode (child process)\n");
+        if (setupAndStartFire() != 0) {
+            fprintf(stderr, "error in setupAndStartFire\n");
+            return 1;
+        }
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
+        for (;;) {
+            pause();
+        }
+
+    } else {
+
+
+        if (setupAndStartFire() != 0) {
+            fprintf(stderr, "error in setupAndStartFire\n");
+            return 1;
+        }
+
+
+        // Things are set up. Just wait for <RETURN> to be pressed.
+        printf("Press <RETURN> to exit and reset LEDs\n");
+        getchar();
+
+        delete image_gen;
+        delete canvas;
+
+    }
     return 0;
 }
